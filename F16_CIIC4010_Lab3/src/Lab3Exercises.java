7aa50fd385a91d29b416de2fb299cc957317a39a
Stephanie <Stephanie@GamerX>
2016-08-31 08:15:46 -0700

    uncommented the setDefaultCloseOperation();.

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -3,7 +3,7 @@ import javax.swing.JFrame;
 public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Stephanie Vargas Villarini");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(590, 280);
 		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
